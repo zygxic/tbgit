bb547054af049c76b9259484e6852f35f91f901e
Igorsandor <igor.sandor@centrum.sk>
2016-11-09 12:17:51 +0100

    Initial commit

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>\
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<div class="tblogocircle">
+	<img src="tblogocircle.png">
+</div>
+
+<div class="fields">
+<form action="">
+<input type="text" name="username" placeholder="Your username"> <br>
+<input type="password" name="password" placeholder="Your password"> <br>
+<input type="submit">
+</form>
+</div>
+
+
+</body>
+</html>
